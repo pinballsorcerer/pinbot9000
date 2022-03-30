27c42a948c4a4b0a4b6933110be3ebcb6b8c4d2e
pinballsorcerer <pinballsorcerer@outlook.com>
2022-03-29 20:19:01 -0700

    Fix most of the formatting issues

--- main.py
+++ main.py
@@ -1,50 +1,52 @@
+'''Starts a Twitch chatbot for my channel'''
 import random
 import irc
 import irc.bot
 
 class TwitchPinBot(irc.bot.SingleServerIRCBot):
+    '''A specific implementation of SingleServerIRCBot for my channel'''
     def __init__(self):
-        with open('C:\\Users\\Pinball\\password.txt', 'r') as f:
-            password = f.readline()
-        irc.bot.SingleServerIRCBot.__init__(self, [("irc.chat.twitch.tv", 6667, password)], "pinbot9000", "pinbot9000")
+        with open('C:\\Users\\Pinball\\password.txt', 'r') as file:
+            password = file.readline()
+        irc.bot.SingleServerIRCBot.__init__(
+            self,
+            [("irc.chat.twitch.tv", 6667, password)],
+            "pinbot9000",
+            "pinbot9000")
         # Note: channel names appear to be case sensitive
         self.channel = "#pinballsorcerer"
         self.greetings = []
 
-    def on_welcome(self, c, e):
-        print("We were welcomed in to the server!")
-        c.join(self.channel)
+    def on_join(self, connection):
+        '''Send an announcement to the channel upon joining'''
+        connection.privmsg(self.channel, "Greetings chat!")
 
-    def on_join(self, c, e):
-        print("We joined a channel! Sending a hello")
-        c.privmsg(self.channel, "Greetings chat!")
-
-    def on_privmsg(self, c, e):
-        print("Private message received")
-
-    def on_pubmsg(self, c, e):
+    def on_pubmsg(self, connection, event):
+        '''Parse each user's message to look for bot interaction'''
         print("Public message received")
-        if (e.arguments[0].lower() == "!marco"):
-            c.privmsg(self.channel, "Polo!")
-        splat = e.arguments[0].split(' ')
+        if event.arguments[0].lower() == "!marco":
+            connection.privmsg(self.channel, "Polo!")
+        splat = event.arguments[0].split(' ')
         if (len(splat) >= 2 and
             splat[0].lower() == "@" + self._nickname.lower()):
             if (splat[1].lower() in ("hi", "hi,", "hello", "hello,")):
                 greeting = random.choice(self.greetings) if self.greetings else ""
-                c.privmsg(self.channel, f"Hi @{e.source.nick}! {greeting}")
+                connection.privmsg(self.channel, f"Hi @{event.source.nick}! {greeting}")
 
-    def on_ping(self, c, e):
+    def on_ping(self, connection, event):
+        '''Reply to ping messages with a pong, as expected by Twitch IRC'''
         print("Received ping")
-        c.send_items("PONG", ":" + e.target)
+        connection.send_items("PONG", ":" + event.target)
 
 def main():
+    '''Setup and start the chatbot'''
     print("hello, I am a chatbot")
     print("starting up")
 
     bot = TwitchPinBot()
 
-    with open('greetings.txt', 'r') as f:
-        bot.greetings = [line.strip() for line in f.readlines()]
+    with open('greetings.txt', 'r') as file:
+        bot.greetings = [line.strip() for line in file.readlines()]
     bot.start()
 
 if __name__ == "__main__":
