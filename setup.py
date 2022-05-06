699539e353264202355db3cd71d38b89880a130a
pinballsorcerer <pinballsorcerer@outlook.com>
2022-05-05 21:44:55 -0700

    Build and store wheel artifacts

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pinbot9000',
+      version='1.0',
+      description='A Twitch chatbot',
+      author='PinballSorcerer',
+      url='https://github.com/pinballsorcerer/pinbot9000/',
+      py_modules = ['main']
+     )
